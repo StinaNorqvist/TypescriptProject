7b0da9e8b5d74102dea767fd84b2d6a6dbdf910c
StinaNorqvist <norqvist.stina@gmail.com>
2024-01-09 18:19:34 +0100

    Fix to mounting menu in test

--- frontend/cypress/component/Menu.cy.tsx
+++ frontend/cypress/component/Menu.cy.tsx
@@ -1,12 +1,20 @@
 import Menu from "../../src/components/Menu";
 import { UserProvider } from "../../src/contexts/UserContext";
+import { CartProvider } from "../../src/contexts/CartContext";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Menu.tsx", () => {
   it("mounts Menu", () => {
     cy.mount(
-      <UserProvider>
-        <Menu />
-      </UserProvider>
+      <>
+        <BrowserRouter>
+          <UserProvider>
+            <CartProvider>
+              <Menu />
+            </CartProvider>
+          </UserProvider>
+        </BrowserRouter>
+      </>
     );
   });
 });
